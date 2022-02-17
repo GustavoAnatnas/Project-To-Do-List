3fbbafd95c00921154b2be81fc0ca4450153ac84
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-17 16:22:19 -0300

    resolve o 7

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function adiciona(){
     // id="lista-tarefas" ==lista
     let texto = document.querySelector('#texto-tarefa').value;
     let lista = document.querySelector('#lista-tarefas').innerHTML;
-
-    lista = lista + "<li>" + texto + "</li>" ;
+    lista = lista + "<li class ='li'>" + texto + "</li>" ;
     document.querySelector("#lista-tarefas").innerHTML = lista;
     document.querySelector('#texto-tarefa').value = '';
+    pintar();
 }
+function pintar() {
+    let li = document.querySelectorAll('.li');
+    for (let index = 0; index < li.length; index++) {
+    lis = li[index];
+    
+    lis.addEventListener('click', function(event){
+        event.target.classList.add('color')
+    });
+    }  
+    }
+    
